fb257ad31e43c89b5dcbc36ee0a887dd35e680fb
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-09-14 20:14:41 +0600

    Event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Hukamma Mokusama';
+export class AppComponent 
+{
+    title : string = "Hukamma Mokusama";
+    imgURL : string = "../favicon.ico";
+    size : number = 300;
+
+    changeImage(event : KeyboardEvent) : void 
+    {
+        this.imgURL = (event.target as HTMLInputElement).value;
+    }
 }
